97f31954854de3678f84471c06a46f9b28ed86b9
Nasser-96 <nasserabunasir96@outlook.com>
2021-10-12 10:32:20 +0300

    Added two functions

--- main.js
+++ main.js
@@ -28,3 +28,32 @@ const characters = [
         gender: 'male',
     },
 ];
+
+function check2(array2, num2){
+    for(let i=0;i<array2.length ; i++){
+        for(let j=i+1;j<array2.length ; j++){
+            if ((array2[i]+array2[j])===num2){
+                console.log("The some of "+array2[i]+" and "+array2[j] +" is equal "+ num2);
+            }
+        }
+    }
+}
+
+function Max(array3){
+    let x = array3[0]
+    for(let i=0;i<array3.length ; i++){
+        if (array3[i]> x){
+            x = array3[i]
+        }
+        
+    }
+    console.log(x);
+}
+
+function stars(str){
+    for(let i=0;i<str ; i++){
+        console.log("\n");
+        for(let j=-1;j<i ; j++)
+        console.log("*");
+    }
+}
